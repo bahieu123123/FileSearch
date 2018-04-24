2ed068dd7013b178d353b0ba8981066b26be30f8
bahieu123123 <ba.hieu0207@gmail.com>
2018-04-24 17:31:22 +0300

    Lesson2

--- src/FileSearch.java
+++ src/FileSearch.java
@@ -95,6 +95,13 @@ public class FileSearch {
         }
         String fileName = args[args.length - 1];
 
+        try{
+            fileSearch.search(fileName, directory, CmdLine.contains("-r"));
+        } catch (Exception e){
+            System.err.println(e.toString());
+            return;
+        }
+
         System.out.println("The name of the found file: " + fileName);
         List<String> result = fileSearch.getResult();
         if (result.size() == 0) System.out.println("There is no files in this directory!");
