c90fb33ad1e8ff9a2edebc7675cbf2fe8b5f343f
bahieu123123 <ba.hieu0207@gmail.com>
2018-05-15 14:34:20 +0300

    Lesson2

--- src/filesearch/FileSearch.java
+++ src/filesearch/FileSearch.java
@@ -1,23 +1,11 @@
 package filesearch;
 
-import org.kohsuke.args4j.Argument;
-import org.kohsuke.args4j.Option;
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class FileSearch {
-    @Option(name = "-r", metaVar = "SubDirect", usage = "Find Subdirectory")
-    private boolean subDirectory;
-
-    @Option(name = "-d", metaVar = "Directory", usage = "Path to the File")
-    private String directory;
-
-    @Argument(metaVar = "FileName", required = true, usage = "File name")
-    private String fileName;
-
     private ArrayList<String> result = new ArrayList<>();
 
     public ArrayList<String> getResult() {
@@ -48,64 +36,5 @@ public class FileSearch {
             } else result.add("Permission denied");
         }
     }
-
-
-
-    /**
-     * find проложить маршрут.
-     * Поиск файла с заданным в командной строке именем в указанной ключом -d директории, по умолчанию в текущей директории.
-     * Ключ -r указывает на необходимость поиска также во всех поддиректориях.
-     *
-     * @param args
-     */
-    public static void main(String[] args) {
-        FileSearch fileSearch = new FileSearch();
-        List<String> cmdLine = Arrays.asList(args);
-        if (cmdLine.contains("find")) {
-            System.out.println("Enter the commands you need and the file name, \n" +
-                    "[-d] - search file in directory \n" +
-                    "[-r] - search file in subdirectories \n");
-            return;
-        }
-
-        File directory = new File(new File("").getAbsolutePath());
-
-        if (cmdLine.contains("-d")) {
-            if (args[args.length - 1].equals("-d"))
-                System.out.println("Directory where you want to check does not exist!");
-            else {
-                String directoryName = cmdLine.get(cmdLine.indexOf("-d") + 1).toString();
-                directory = new File(directoryName);
-            }
-            if (!directory.isDirectory()) System.out.println("Directory where you want to check does not exist!");
-            else System.out.println("Directory where you want to check the presence of a file: " + directory);
-        }
-        if (args[0].equals("-r")) {
-            String fileName = args[args.length - 1];
-            fileSearch.search(fileName, directory, cmdLine.contains("-r"));
-
-            System.out.println("The name of the found file: " + fileName);
-            List<String> result = fileSearch.getResult();
-            System.out.println("\nFound " + " result:\n");
-            for (String str : result) {
-                System.out.println(str);
-            }
-        } else {
-            if (args.length <= 2 || args.length > 4) System.out.println("File name or directory name not yet written!");
-            else {
-                String fileName = args[args.length - 1];
-                fileSearch.search(fileName, directory, cmdLine.contains("-r"));
-
-                System.out.println("The name of the found file: " + fileName);
-                List<String> result = fileSearch.getResult();
-                System.out.println("\nFound " + " result:\n");
-                for (String str : result) {
-                    System.out.println(str);
-                }
-            }
-        }
-    }
-
-
 }
 
---
+++ src/filesearch/Launcher.java
@@ -0,0 +1,69 @@
+package filesearch;
+
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+public class Launcher {
+    @Option(name = "-r", metaVar = "SubDirect", usage = "Find Subdirectory")
+    private boolean subDirectory;
+
+    @Option(name = "-d", metaVar = "Directory", usage = "Path to the File")
+    private String directory;
+
+    @Argument(metaVar = "FileName", required = true, usage = "File name")
+    private String fileName;
+
+    /**
+     * find проложить маршрут.
+     * Поиск файла с заданным в командной строке именем в указанной ключом -d директории, по умолчанию в текущей директории.
+     * Ключ -r указывает на необходимость поиска также во всех поддиректориях.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        new Launcher().launch(args);
+    }
+
+    private void launch(String[] args) {
+        CmdLineParser parser = new CmdLineParser(this);
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
+            System.err.println("find [-r] [-d directory] filename.txt");
+            parser.printUsage(System.err);
+            return;
+        }
+        FileSearch fileSearch = new FileSearch();
+        List<String> cmdLine = Arrays.asList(args);
+        File directory = new File(new File("").getAbsolutePath());
+
+        if (cmdLine.contains("-d")) {
+            if (args[args.length - 1].equals("-d"))
+                System.out.println("Directory where you want to check does not exist!");
+            else {
+                String directoryName = cmdLine.get(cmdLine.indexOf("-d") + 1).toString();
+                directory = new File(directoryName);
+            }
+            if (!directory.isDirectory()) System.out.println("Directory where you want to check does not exist!");
+            else System.out.println("Directory where you want to check the presence of a file: " + directory);
+        }
+        String fileName = args[args.length - 1];
+        fileSearch.search(fileName, directory, cmdLine.contains("-r"));
+
+        System.out.println("The name of the found file: " + fileName);
+        List<String> result = fileSearch.getResult();
+        System.out.println("\nFound " + " result:\n");
+        for (String str : result) {
+            System.out.println(str);
+        }
+
+    }
+}
+
